81b1cb4be40d6fb26a93d6749eb1ccbdd34362fa
basil roy <basil.k.roy@hotmail.com>
2020-06-05 00:21:12 +0100

    imported deck of cards in and modifying its pixels

---
+++ src/components/cards.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Cards extends Component {
+  flop = () => {};
+  turn = () => {};
+  river = () => {};
+  dealhands = () => {};
+  render() {
+    return <div>HEloo poker</div>;
+  }
+}
+export default Cards;
--- src/components/home.js
+++ src/components/home.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import Cards from "./cards";
 class Home extends Component {
   render() {
-    return <div>HEloo poker</div>;
+    return (
+      <div>
+        <div>PokerStars 2.0</div>
+      </div>
+    );
   }
 }
 export default Home;
